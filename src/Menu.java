12e8159dba986a79d4924e43274bfcf24e73c66d
narayan99883 <pandeyavi1@gmail.com>
2021-12-30 12:36:45 -0500

    Changes to Menu

--- src/Menu.java
+++ src/Menu.java
@@ -8,27 +8,25 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-
 public class Menu {
 
-	//running the menu which the user uses
+	// running the menu which the user uses
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 		Garage garage = new Garage();
 		Scanner input = new Scanner(System.in);
-		
-		while(true) {
-			
+
+		while (true) {
+
 			System.out.println("Available commands: insert, delete, clear, sort, info, space, other, end");
 			String choice = input.nextLine();
-			
+
 			if (choice.equals("insert")) {
 				garage.insert();
 			} else if (choice.equals("delete")) {
 				garage.delete();
-			} else if (choice.equals("clear") ) {
+			} else if (choice.equals("clear")) {
 				garage.clearSection();
 			} else if (choice.equals("sort")) {
 				garage.sort();
@@ -37,18 +35,18 @@ public class Menu {
 			} else if (choice.equals("space")) {
 				garage.howMuchSpace();
 			} else if (choice.equals("other")) {
-				
+
 				System.out.println("Available commands: dislpay, senduserguide, senddisplay, sendlist");
 				String secchoice = input.nextLine();
-				
+
 				if (secchoice.equals("display")) {
-					DisplayImage abc=new DisplayImage();
+					DisplayImage abc = new DisplayImage();
 				} else if (secchoice.equals("senduserguide")) {
 					System.out.println("To what email address?");
 					String email = input.nextLine();
 					JavaMailUtil.sendGuideMail(email);
 				} else if (secchoice.equals("senddisplay")) {
-					System.out.println("To what email address?"); 
+					System.out.println("To what email address?");
 					String email = input.nextLine();
 					JavaMailUtil.sendDisplayMail(email);
 				} else if (secchoice.equals("sendlist")) {
@@ -59,26 +57,20 @@ public class Menu {
 				} else {
 					System.out.println("invalid command");
 				}
-				
+
 			} else if (choice.equals("end")) {
 				System.out.println("Program ending");
 				System.exit(0);
-			} else if (choice.equals("filee")) { //FOR TESTING PURPOSES
-				garage.createFilee();
-			} else if (choice.equals("testt")) {
-				garage.testWrite();
 			}
-			
+
 			else {
 				System.out.println("invalid command");
 			}
-			
+
 			System.out.println("\n");
-			
+
 		}
 
 	}
 
 }
-
-
